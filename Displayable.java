aeb989d341649a964727ca7b5fb5178933081de4
hyy1018 <hyy10188@gmail.com>
2026-08-30 15:49:49 +0800

    Update Displayable.java

--- Displayable.java
+++ Displayable.java
@@ -1,11 +1,10 @@
-package asgm;
+package langkawigraphsystem;
 
-/**
- * Any vertex that wants to be drawn by GraphView must implement this,
- * as taught in Practical 9B (Graph Visualization section).
- */
 public interface Displayable {
-    public int getX(); // Get x-coordinate of the vertex
-    public int getY(); // Get y-coordinate of the vertex
-    public String getName(); // Get display name of the vertex
+
+    public int getX();
+
+    public int getY();
+
+    public String getName();
 }
